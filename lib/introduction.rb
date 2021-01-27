748d69821f5f6adedd9fe147295b730347cf39a8
Christopher Dajer <chrisdajer@gmail.com>
2021-01-27 19:22:36 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -22,11 +22,11 @@ introduction_with_language("Christopher", "ruby")
 
 #introduction_with_language_optional
 
-def introduction_with_language_optional (name, language = "ruby")
+def introduction_with_language_optional (name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
 
-introduction_with_language_optional ("Christopher" "ruby")
+introduction_with_language_optional ("Christopher")
 
 
 
